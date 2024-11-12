521472b026ca1adb834a439a73b4343ff14cdc6c
Varad  Milind  Marathe <113687091+VaradMarathe24@users.noreply.github.com>
2024-11-12 22:20:38 +0530

    Update SJP NonPreemptive.java

--- SJP NonPreemptive.java
+++ SJP NonPreemptive.java
@@ -1,74 +1,115 @@
-Program: SJF Non Preemptive 
+import java.util.Arrays;
 import java.util.Scanner;
- class SJFNP {
-    int pid, burstTime, arrivalTime, waitingTime, turnaroundTime;
+
+class SJFNProcess {
+    int pid, burstTime, arrivalTime, waitingTime = 0, turnaroundTime = 0;
+    boolean isCompleted = false;
+    int startTime = 0; // New: to store start time
+    int endTime = 0;   // New: to store end time
+
     public SJFNProcess(int pid, int burstTime, int arrivalTime) {
         this.pid = pid;
         this.burstTime = burstTime;
         this.arrivalTime = arrivalTime;
     }
- }
- public class Main {
+}
+
+public class SJFNonPreemptive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter number of processes: ");
         int n = sc.nextInt();
         SJFNProcess[] processes = new SJFNProcess[n];
+
         for (int i = 0; i < n; i++) {
             System.out.print("Enter arrival time and burst time for process " + (i + 1) + ": ");
             int at = sc.nextInt();
             int bt = sc.nextInt();
             processes[i] = new SJFNProcess(i + 1, bt, at);
         }
-        // Sort processes by arrival time and burst time
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (processes[j].burstTime > processes[j + 1].burstTime) {
-                    SJFNProcess temp = processes[j];
-                    processes[j] = processes[j + 1];
-                    processes[j + 1] = temp;
+
+        // Sort processes based on arrival time initially
+        Arrays.sort(processes, (p1, p2) -> Integer.compare(p1.arrivalTime, p2.arrivalTime));
+
+        int totalTime = 0, completedProcesses = 0;
+        float totalWT = 0, totalTAT = 0;
+
+        while (completedProcesses < n) {
+            int shortestJobIndex = -1;
+            int minBurstTime = Integer.MAX_VALUE;
+
+            // Find the process with the shortest burst time among those that have arrived
+            for (int i = 0; i < n; i++) {
+                if (!processes[i].isCompleted && processes[i].arrivalTime <= totalTime) {
+                    if (processes[i].burstTime < minBurstTime) {
+                        minBurstTime = processes[i].burstTime;
+                        shortestJobIndex = i;
+                    }
                 }
             }
+
+            if (shortestJobIndex == -1) {
+                // If no process has arrived yet, move time forward
+                totalTime++;
+                continue;
+            }
+
+            // Process the selected job
+            SJFNProcess currentProcess = processes[shortestJobIndex];
+            currentProcess.startTime = totalTime; // Set start time
+            currentProcess.waitingTime = totalTime - currentProcess.arrivalTime;
+            totalTime += currentProcess.burstTime;
+            currentProcess.endTime = totalTime; // Set end time
+            currentProcess.turnaroundTime = currentProcess.waitingTime + currentProcess.burstTime;
+            currentProcess.isCompleted = true;
+            completedProcesses++;
+
+            // Accumulate total waiting and turnaround times
+            totalWT += currentProcess.waitingTime;
+            totalTAT += currentProcess.turnaroundTime;
         }
-        int totalTime = 0, totalWT = 0, totalTAT = 0;
+
+        // Print Gantt Chart with start and end times
+        System.out.println("\nGantt Chart:");
         for (SJFNProcess p : processes) {
-            p.waitingTime = totalTime - p.arrivalTime;
-            totalTime += p.burstTime;
-            p.turnaroundTime = p.waitingTime + p.burstTime;
-            totalWT += p.waitingTime;
-            totalTAT += p.turnaroundTime;
+            System.out.print("| P" + p.pid + " ");
         }
-        // Print Gantt Chart
-        System.out.println("Gantt Chart: ");
+        System.out.println("|");
         for (SJFNProcess p : processes) {
-            System.out.print("P" + p.pid + " ");
+            System.out.print(p.startTime + "\t ");
         }
-        System.out.println("\n");
+        System.out.print(totalTime + "\n");
+
         // Print process details
-        System.out.println("Process\tArrival\tBurst\tWaiting\tTurnaround");
+        System.out.println("\nProcess\tArrival\tBurst\tWaiting\tTurnaround");
         for (SJFNProcess p : processes) {
-            System.out.println("P" + p.pid + "\t" + p.arrivalTime + "\t" + p.burstTime + "\t" + p.waitingTime + "\t" + p.turnaroundTime);
+            System.out.println("P" + p.pid + "\t" + p.arrivalTime + "\t" + p.burstTime + "\t" +
+                               p.waitingTime + "\t" + p.turnaroundTime);
         }
-        System.out.println("Average Waiting Time: " + (totalWT / (float) n));
-        System.out.println("Average Turnaround Time: " + (totalTAT / (float) n));
+
+        // Print average waiting time and turnaround time
+        System.out.println("Average Waiting Time: " + (totalWT / n));
+        System.out.println("Average Turnaround Time: " + (totalTAT / n));
+
         sc.close();
     }
- }
-Output:
-Enter number of processes: 5
-Enter arrival time and burst time for process 1: 7 4
-Enter arrival time and burst time for process 2: 5 3 
-Enter arrival time and burst time for process 3: 2 7
-Enter arrival time and burst time for process 4: 3 5
-Enter arrival time and burst time for process 5: 2 4
-Gantt Chart: 
-P2 P1 P5 P4 P3 
+}
+
+/*
+Enter number of processes: 3
+Enter arrival time and burst time for process 1: 1 2
+Enter arrival time and burst time for process 2: 0 5
+Enter arrival time and burst time for process 3: 2 3
+
+Gantt Chart:
+| P2 | P1 | P3 |
+0        5       7       10
 
 Process Arrival Burst   Waiting Turnaround
-P2      5       3       -5      -2
-P1      7       4       -4      0
-P5      2       4       5       9
-P4      3       5       8       13
-P3      2       7       14      21
-Average Waiting Time: 3.6
-Average Turnaround Time: 8.2
+P2      0       5       0       5
+P1      1       2       4       6
+P3      2       3       5       8
+Average Waiting Time: 3.0
+Average Turnaround Time: 6.3333335  
+ */
