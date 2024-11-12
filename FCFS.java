4ddddaa40bc97acbc92a26f2c7d4e234a142bc3f
Varad  Milind  Marathe <113687091+VaradMarathe24@users.noreply.github.com>
2024-11-12 22:19:35 +0530

    Update FCFS.java

--- FCFS.java
+++ FCFS.java
@@ -1,74 +1,87 @@
-Program: FCFS 
-import java.io.*;
 import java.util.Scanner;
-public class FCFS {
-	public static void main(String[] args) {
-		int i,no_p,bt[],TT[],WT[];
-		float avg_wt=0,avg_TT=0;
-		bt=new int[50];
-		TT=new int[50];
-		WT=new int[50];
-		WT[0]=0;
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the number of processes:");
-		no_p=s.nextInt();
-		System.out.println("Enter the burst time:");
-		for(i=0;i<no_p;i++) {
-		    System.out.println("\t P"+(i+1)+": ");
-		    bt[i]=s.nextInt();
-		}
-		for(i=1;i<no_p;i++) {
-		     WT[i]=WT[i-1]+bt[i-1];
-             		     avg_wt+=WT[i];
-		}
-        avg_wt/=no_p;
-        for(i=0;i<no_p;i++)
-        {
-        TT[i]=WT[i]+bt[i];
-        avg_TT+=TT[i];
-        }
-        avg_TT/=no_p;
-		System.out.println("\n****************************************************************");
-        System.out.println("\tProcesses:");
-        System.out.println("****************************************************************");
-        System.out.println("Process\tBurst Time\tWaiting Time\tTurn Around Time");
-        for(i=0;i<no_p;i++)
-        {
-        System.out.println("\tP"+(i+1)+"\t"+bt[i]+"\t\t  "+WT[i]+"\t\t "+TT[i]);
-        }
-        System.out.println("\n----------------------------------------------------------------");
-        System.out.println("\nAverage waiting time : "+avg_wt);
-        System.out.println("\nAverage Turn Around time :"+avg_TT+"\n");
-	}
+class MProcess {
+int pid, burstTime, arrivalTime, waitingTime, turnaroundTime;
+public MProcess(int pid, int burstTime, int arrivalTime) 
+{
+this.pid = pid;
+this.burstTime = burstTime;
+this.arrivalTime = arrivalTime;
 }
-Output:
-Enter the number of processes:
-5
-Enter the burst time:
-         P1: 
-2
-         P2: 
-3
-         P3: 
-5
-         P4: 
-7
-         P5: 
-11
+}
 
-****************************************************************
-        Processes:
-****************************************************************
-Process Burst Time      Waiting Time    Turn Around Time
-        P1      2                 0              2
-        P2      3                 2              5
-        P3      5                 5              10
-        P4      7                 10             17
-        P5      11                17             28
+public class FCFS {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter number of processes: ");
+int n = sc.nextInt();
 
-----------------------------------------------------------------
+MProcess[] processes = new MProcess[n];
+for (int i = 0; i < n; i++) {
+System.out.print("Enter arrival time and burst time for process " + (i + 1) + ": ");
+int at = sc.nextInt();
+int bt = sc.nextInt();
+processes[i] = new MProcess(i + 1, bt, at);
+}
 
-Average waiting time : 6.8
+// Sort processes based on arrival time
+for (int i = 0; i < n - 1; i++) {
+for (int j = 0; j < n - i - 1; j++) {
+if (processes[j].arrivalTime > processes[j + 1].arrivalTime) {
+MProcess temp = processes[j];
+processes[j] = processes[j + 1];
+processes[j + 1] = temp;
+}
+}
+}
 
-Average Turn Around time :12.4
+// Calculate waiting time and turnaround time
+int totalTime = 0, totalWT = 0, totalTAT = 0;
 
+for (MProcess p : processes) {
+p.waitingTime = totalTime - p.arrivalTime;
+totalTime += p.burstTime;
+p.turnaroundTime = p.waitingTime + p.burstTime;
+totalWT += p.waitingTime;
+totalTAT += p.turnaroundTime;
+}
+
+// Print Gantt Chart
+System.out.println("Gantt Chart: ");
+for (MProcess p : processes) {
+System.out.print("P" + p.pid + " ");
+}
+System.out.println("\n");
+
+// Print process details
+System.out.println("Process\tArrival\tBurst\tWaiting\tTurnaround");
+for (MProcess p : processes) {
+System.out.println("P" + p.pid + "\t" + p.arrivalTime + "\t" + p.burstTime + "\t" +
+p.waitingTime + "\t" + p.turnaroundTime);
+}
+
+System.out.println("Average Waiting Time: " + (totalWT / (float) n));
+System.out.println("Average Turnaround Time: " + (totalTAT / (float) n));
+sc.close();
+}
+}
+
+
+/*
+ Enter number of processes: 5
+Enter arrival time and burst time for process 1: 0 4
+Enter arrival time and burst time for process 2: 1 3
+Enter arrival time and burst time for process 3: 2 1
+Enter arrival time and burst time for process 4: 3 2
+Enter arrival time and burst time for process 5: 4 5
+Gantt Chart: 
+P1 P2 P3 P4 P5 
+
+Process Arrival Burst   Waiting Turnaround
+P1      0       4       0       4
+P2      1       3       3       6
+P3      2       1       5       6
+P4      3       2       5       7
+P5      4       5       6       11
+Average Waiting Time: 3.8
+Average Turnaround Time: 6.8
+ */
